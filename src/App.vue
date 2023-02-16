175abe9acab27c6007c564e5b58a9b06cf3e8e90
alonzojoe <medusazxc123@gmail.com>
2023-02-16 23:06:37 +0800

    setup vue-router

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink,RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+  <div class="div">
+    <nav>
+      <li><RouterLink to="/">Home</RouterLink></li>
+      <li><RouterLink to="/test">Test</RouterLink></li>
+    </nav>
+    <div>
+      <RouterView />
+    </div>
+  
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue"
+import TestPage from "../views/TestPage.vue"
+
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView
+        },
+        {
+            path: "/test",
+            name: "test",
+            component: TestPage
+        },
+   
+    ]
+})
+
+export default router
